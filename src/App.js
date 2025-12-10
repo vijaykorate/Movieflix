e7e5d287a43a39a72af0c629974533990b2e562e
Vijay Korate <vijaykorate18@gmail.com>
2025-12-10 15:38:06 +0530

    using useEffect  async fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,9 +50,22 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "162f135";
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  useEffect(function () {
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=Inception`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+  }, []);
+
   return (
     <>
       <NavBar>
